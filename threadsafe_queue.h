afb0c602a569948626f94cf532677d63197f3f63
pw-ethan <weilaiwen92@126.com>
2017-06-23 09:03:32 +0800

    update thread safe queue use unique_ptr

--- threadsafe_queue.h
+++ threadsafe_queue.h
@@ -10,7 +10,8 @@ template<typename T>
 class threadsafe_queue {
 private:
     mutable std::mutex mut;
-    std::queue<T> data_queue;
+    // std::queue<T> data_queue; // version 1
+    std::queue<std::shared_ptr<T>> data_queue;
     std::condition_variable data_cond;
 
 public:
@@ -22,8 +23,10 @@ public:
     threadsafe_queue& operator=(const threadsafe_queue&) = delete; // don't allow operator=
 
     void push(T new_value) {
+        std::shared_ptr<T> data(std::make_shared<T>(std::move(new_value)));
         std::lock_guard<std::mutex> lk(mut);
-        data_queue.push(std::move(new_value)); // what?
+//        data_queue.push(std::move(new_value));
+        data_queue.push(data);
         data_cond.notify_one();
     }
 
@@ -32,7 +35,8 @@ public:
         if(data_queue.empty()) {
             return false;
         }
-        value = std::move(data_queue.front());
+//        value = std::move(data_queue.front());
+        value = std::move(*data_queue.front());
         data_queue.pop();
         return true;
     }
@@ -41,7 +45,8 @@ public:
         if(data_queue.empty()) {
             return std::shared_ptr<T>();
         }
-        std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
+//        std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
+        std::shared_ptr<T> res = data_queue.front();
         data_queue.pop();
         return res;
     }
@@ -49,13 +54,15 @@ public:
     void wait_and_pop(T& value) {
         std::unique_lock<std::mutex> lk(mut);
         data_cond.wait(lk, [this]{return !data_queue.empty();});
-        value = std::move(data_queue.front());
+//        value = std::move(data_queue.front());
+        value = std::move(*data_queue.front());
         data_queue.pop();
     }
     std::shared_ptr<T> wait_and_pop() {
         std::unique_lock<std::mutex> lk(mut);
         data_cond.wait(lk, [this]{return !data_queue.empty();});
-        std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
+//        std::shared_ptr<T> res(std::make_shared<T>(std::move(data_queue.front())));
+        std::shared_ptr<T> res = data_queue.front();
         data_queue.pop();
         return res;
     }
